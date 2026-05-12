aac86c20ae3230ecc0f6d79bb94c179cd0743e27
Shreesta Khatiwada <khatiwadasrista3@gmail.com>
2026-05-12 08:27:01 +0545

    Week 2 completed

--- Week2/script.js
+++ Week2/script.js
@@ -1,36 +1,39 @@
-function calculateResult() {
 
-    let totalMarks =
-        Number(document.getElementById("m1").value) +
-        Number(document.getElementById("m2").value) +
-        Number(document.getElementById("m3").value) +
-        Number(document.getElementById("m4").value) +
-        Number(document.getElementById("m5").value) +
-        Number(document.getElementById("m6").value) +
-        Number(document.getElementById("m7").value) +
-        Number(document.getElementById("m8").value);
+function getNumbers() {
+    let number1 = parseFloat(document.getElementById("num1").value);
+    let number2 = parseFloat(document.getElementById("num2").value);
 
+    return [number1, number2];
+}
 
-    //let totalText = document.getElementById("total");
-    //let resultText = document.getElementById("result");
+function add() {
+    let [number1, number2] = getNumbers();
+    let result = number1 + number2;
 
-    document.getElementById("total").innerHTML = "Total Marks: " + totalMarks + " / 800";
+    document.getElementById("result").innerHTML = "Addition Result: " + result;
+}
 
-    if (totalMarks >= 700) {
-        document.getElementById("result").innerHTML = "DISTINCTION";
-        document.getElementById("result").style.color = "green";
-    }
-    else if (totalMarks >= 600) {
-        document.getElementById("result").innerHTML = "PASS – First Division";
-    }
-    else if (totalMarks >= 500) {
-        document.getElementById("result").innerHTML = "PASS – Second Division";
-    }
-    else if (totalMarks >= 400) {
-        document.getElementById("result").innerHTML = "PASS – Third Division";
-    }
-    else {
-        document.getElementById("result").innerHTML = "FAIL";
-        document.getElementById("result").style.color = "red";
+function subtract() {
+    let [number1, number2] = getNumbers();
+    let result = number1 - number2;
+
+    document.getElementById("result").innerHTML = "Subtraction Result: " + result;
+}
+
+function multiply() {
+    let [number1, number2] = getNumbers();
+    let result = number1 * number2;
+
+    document.getElementById("result").innerHTML = "Multiplication Result: " + result;
+}
+
+function divide() {
+    let [number1, number2] = getNumbers();
+
+    if (number2 === 0) {
+        document.getElementById("result").innerHTML = "Cannot divide by zero";
+    } else {
+        let result = number1 / number2;
+        document.getElementById("result").innerHTML = "Division Result: " + result;
     }
 }
