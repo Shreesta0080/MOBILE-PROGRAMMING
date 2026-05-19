45aa23fa71592ee61bd6d7e288757f5bcaf8abec
Shreesta Khatiwada <khatiwadasrista3@gmail.com>
2026-05-19 08:22:10 +0545

    Added Week 3.1 jQuery animation project

--- Week2.2/script.js
+++ Week2.2/script.js
@@ -1,22 +1,16 @@
 let balance = 10000;
 let correctPin = "1234";
-
 function deposit() {
 
     let amount = Number(document.getElementById("amount").value);
-
     if (amount % 100 != 0) {
         document.getElementById("message").innerHTML =
             "Amount must be multiple of 100";
         return;
     }
-
     let pin = prompt("Enter PIN");
-
     if (pin == correctPin) {
-
         balance = balance + amount;
-
         document.getElementById("balance").innerHTML =
             "Current Balance: Rs. " + balance;
 
@@ -28,9 +22,7 @@ function deposit() {
             "Incorrect PIN";
     }
 }
-
 function withdraw() {
-
     let amount = Number(document.getElementById("amount").value);
 
     if (amount % 100 != 0) {
@@ -38,20 +30,14 @@ function withdraw() {
             "Amount must be multiple of 100";
         return;
     }
-
     let pin = prompt("Enter PIN");
-
     if (pin == correctPin) {
-
         if (amount > balance) {
-
             document.getElementById("message").innerHTML =
                 "Insufficient Balance";
         }
         else {
-
             balance = balance - amount;
-
             document.getElementById("balance").innerHTML =
                 "Current Balance: Rs. " + balance;
 
